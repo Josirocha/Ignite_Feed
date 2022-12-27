9b0cfde9925e313044af187fab3eda4a5e36651d
Josiane Rocha <josi1992@gmail.com>
2022-12-27 01:41:32 -0300

    feat: Estrutura da aplicação - Finalizando o Sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Header } from "./components/Header";
 import Post from "./Post";
 
+import styles from './app.module.css'
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Post author="Josiane Rocha" content="Lorem ipsum dolor sit" />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Josiane Rocha" content="Lorem ipsum dolor sit" />
 
-      <Post author="Guilherme Santos" content="Lorem ipsum dolor sit" />
+          <Post author="Guilherme Santos" content="Lorem ipsum dolor sit" />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import {PencilLine} from 'phosphor-react'
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img clasName={styles.cover}
+                src="https://images.unsplash.com/photo-1665686376173-ada7a0031a85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50"
+            />
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Josirocha.png" />
+
+
+                <strong>Josiane Rocha</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar Seu Perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
