d305bf676b3943b3c4e29887904296e1165ac67e
Josiane Rocha <josi1992@gmail.com>
2023-02-09 16:22:42 -0300

    feat:  Os motores do React - Entendendo a key

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import styles from './Sidebar.module.css'
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img clasName={styles.cover}
+            <img className={styles.cover}
                 src="https://images.unsplash.com/photo-1665686376173-ada7a0031a85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50"
             />
             <div className={styles.profile}>
