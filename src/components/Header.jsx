6bab5ee17a64198bee03490d1a9ede53179e2fad
Josiane Rocha <josi1992@gmail.com>
2024-11-06 00:59:52 -0300

    fix: bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
 import styles from "./Header.module.css";
-import igniteLogo from "../assents/ignite-logo.svg";
 
 export function Header() {
     return (
-        <header className={styles.header}>
-            <img src={igniteLogo} alt="Logotipo do Ignite" />
-        </header>
+        <header className={styles.header}/>
     );
 }
